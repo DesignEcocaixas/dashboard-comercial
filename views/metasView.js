de1938148d974154b54a40b10e7c9cf60ff932e6
DesignEcocaixas <designecocaixasba@gmail.com>
2026-02-19 16:38:44 -0300

    Update metasView.js

--- views/metasView.js
+++ views/metasView.js
@@ -192,6 +192,7 @@ function metasView(funcionarios, metaAtual, vendas, usuarioLogado) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
 
 /* ========================= */
