18e95ea3349c80c37031d98ba54ce50fd270f430
David Silva de Souza <davidsilva15@vps61302.publiccloud.com.br>
2026-02-19 16:45:16 -0300

    Ajustes produção VPS porta 3052

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
-    user: 'root',
-    password: '1234',
+    user: 'metas_user',
+    password: '23!Bestdavidx',
     database: 'sistema_metas'
 });
 
